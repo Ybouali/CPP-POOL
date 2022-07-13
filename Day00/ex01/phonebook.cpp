c32a58e44ea25eb8c0485d810f67389980fa3076
Ybouali <89746106+Ybouali@users.noreply.github.com>
2022-07-13 12:07:46 +0100

    WORKING ON SEARCH CONTACT

--- Day00/ex01/phonebook.cpp
+++ Day00/ex01/phonebook.cpp
@@ -1,10 +1,21 @@
 #include "./Phonebook.class.hpp"
 
+int is_str_of_numbers(std::string str) {
+    
+    for (int i = 0; i < (int)str.length(); i++)
+    {
+        if (!isalnum(str[i]))
+            return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     Contact phonebook[MAX_CONTACT];
     std::string cmd;
     int nContact = -1;
+    std::string n;
     while (1)
     {
         std::cout << "(ADD, SEARCH, EXIT): ";
@@ -21,10 +32,15 @@ int main()
         {
             while (1)
             {
-                std::cout << "Please enter a contact number.";
-                if (!std::getline(std::cin, cmd))
-                    std::cout << "You did not enter a number of contact" << std::endl;
+                std::cout << "Please enter a contact number. :: ";
+                std::getline(std::cin, n);
+                if (!is_str_of_numbers(n))
+                {
+                    int number = atoi(n.c_str());
+                    std::cout << number << std::endl;
+                }
             }
+
         }
         
     }
