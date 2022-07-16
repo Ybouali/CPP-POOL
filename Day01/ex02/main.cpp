ab19dfc4b9859f83149bc38cf7d028a52caad8f9
Ybouali <89746106+Ybouali@users.noreply.github.com>
2022-07-16 17:37:54 +0100

    DAY01/EX02 DONE

---
+++ Day01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+    std::string string1 = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string1;
+    std::string &stringREF = string1;
+
+    std::cout << "The address of the string         :: " << &string1 << std::endl;
+    std::cout << "The address held by stringRTP     :: " << stringPTR << std::endl;
+    std::cout << "The address held by stringREF     :: " << &stringREF << std::endl;
+    
+    std::cout << "The value of the string variable  :: " << string1 << std::endl;
+    std::cout << "The value pointed to by stringRTP :: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringRTP :: " << stringREF << std::endl;
+
+
+    return 0;
+}
